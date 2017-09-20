709a704834db59b5b9413414dc74b2952514a758
Matthew Harrigan <harrigan@stanford.edu>
2017-09-19 22:35:06 -0700

    Build on windows

--- README.md
+++ README.md
@@ -8,5 +8,27 @@ My personal website hosted using Github pages with Jekyll
 Building
 --------
 
+Make sure these are installed:
+
+ - Node.js
+ - Ruby
+ - Python
+ 
+Then get dependencies
+
+    gem install bundler
+    bundle install
     npm install
+    
+and run a little script to compile the css and
+copy things over to the `assets/` directory
+
     python build.py
+
+Host a dev version of the site with
+
+    ./jekyll.sh
+    
+which just does
+
+    bundle exec jekyll serve --incremental
--- build.py
+++ build.py
@@ -1,9 +1,17 @@
-from subprocess import run
+from subprocess import run as _run
+import sys
 import shutil
 import re
+import os
+
+if sys.platform == 'win32':
+    def run(args):
+        return _run(args, shell=True)
+else:
+    run = _run
 
 run(['scss', '--load-path', 'node_modules/bootstrap/scss', 'scss/mph.scss', 'assets/style.css'])
-run(['node_modules/.bin/postcss', '--use', 'autoprefixer', '--replace', 'assets/style.css'])
+run([os.path.join('node_modules','.bin', 'postcss'), '--use', 'autoprefixer', '--replace', 'assets/style.css'])
 
 with open('node_modules/pygments-css/default.css') as inf, open('assets/pygments.css', 'w') as outf:
     outf.write(re.sub(r'codehilite', r'highlight', inf.read()))
